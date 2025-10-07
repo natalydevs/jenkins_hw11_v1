86a4bf5d0ad1aed64d27a74587d37cca6ffd83ec
Nataly Kostiuchenko <natcheshirecats@yandex.ru>
2025-10-07 17:53:20 +0300

    Adding driver_path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,7 @@
-# tests/conftest.py
 import os
 import pytest
 from selene import browser
-from selenium.webdriver import ChromeOptions
+from selenium.webdriver import ChromeOptions, ChromeService
 
 @pytest.fixture(scope='function', autouse=True)
 def set_browser():
@@ -16,12 +15,15 @@ def set_browser():
     if chrome_bin:
         options.binary_location = chrome_bin
 
+    driver_path = os.path.join(os.getenv('WORKSPACE', os.getcwd()), 'chromedriver-linux64', 'chromedriver')
+
+    service = ChromeService(executable_path=driver_path)
+
     browser.config.driver_name = 'chrome'
     browser.config.driver_options = options
+    browser.config.driver_service = service
 
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
     browser.config.timeout = 10
     yield
     browser.quit()
