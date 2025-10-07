465dd5e60c9c822f521779433e0818fbd767e470
Nataly Kostiuchenko <natcheshirecats@yandex.ru>
2025-10-07 18:44:42 +0300

    Adding changes to conftest to fix the errors in Jenkins(v3).

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,17 +8,26 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 @pytest.fixture(scope='function', autouse=True)
 def set_browser():
     options = ChromeOptions()
+    # устойчивый набор флагов для контейнеров/Alpine
     options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-software-rasterizer')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--no-zygote')
     options.add_argument('--window-size=1920,1080')
+    # на всякий пожарный для новых версий хрома/драйвера
+    options.add_argument('--remote-allow-origins=*')
 
+    # Явно укажем бинарь chromium, который мы ставим в Execute shell
     chrome_bin = os.getenv('CHROME_BIN')
     if chrome_bin:
         options.binary_location = chrome_bin
 
+    # Явно укажем путь до chromedriver и включим лог
     driver_path = os.getenv('CHROMEDRIVER')
-    service = ChromeService(executable_path=driver_path) if driver_path else None
+    service = ChromeService(executable_path=driver_path, log_output="chromedriver.log") if driver_path else None
 
     browser.config.driver_name = 'chrome'
     browser.config.driver_options = options
@@ -27,5 +36,7 @@ def set_browser():
 
     browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 10
+
     yield
+
     browser.quit()
