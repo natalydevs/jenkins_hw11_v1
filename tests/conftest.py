01fe29921ab26d15bebcf603c009b816dcac9ddb
Nataly Kostiuchenko <natcheshirecats@yandex.ru>
2025-10-07 18:13:55 +0300

    Adding changes to conftest to fix the errors in Jenkins.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
-import os
 import pytest
 from selene import browser
-from selenium.webdriver import ChromeOptions, ChromeService
+from selenium.webdriver import ChromeOptions
 
 @pytest.fixture(scope='function', autouse=True)
 def set_browser():
@@ -11,18 +10,8 @@ def set_browser():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
 
-    chrome_bin = os.getenv('CHROME_BIN')
-    if chrome_bin:
-        options.binary_location = chrome_bin
-
-    driver_path = os.path.join(os.getenv('WORKSPACE', os.getcwd()), 'chromedriver-linux64', 'chromedriver')
-
-    service = ChromeService(executable_path=driver_path)
-
     browser.config.driver_name = 'chrome'
     browser.config.driver_options = options
-    browser.config.driver_service = service
-
     browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 10
     yield
